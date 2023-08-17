ce27e95968041341eb4d6c7ab877771d4a7566ca
Jingzhuo <tianboyd@Tians-MacBook-Pro.local>
2023-08-17 15:02:57 -0700

    css

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,8 +1,15 @@
+import "./ExpenseItems.css"
 function ExpenseItems(){
     return(
-        <h2>
-            Expense Items
-        </h2>
+        <div className = "expense-item">
+            <div>Aug 17th 2023</div>
+            <div className = "expense-item__description"></div>
+            <h2>
+                Expense Items
+            </h2>
+            <div className = "expense-item__price">$69.73</div>
+        </div>
+
     )
 }
 export default ExpenseItems
