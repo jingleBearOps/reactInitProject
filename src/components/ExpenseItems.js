2dad9d5fd03fb5748ff7c5c3e0389781c92aa2c1
Jingzhuo <tianboyd@Tians-MacBook-Pro.local>
2023-08-17 16:14:52 -0700

    component wrapper

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,18 +1,18 @@
 import "./ExpenseItems.css"
-
+import Card from "./Card"
 function ExpenseItems(props){
     const expenseDate = new Date(2023, 8, 17)
     const expenseTitle = 'mushrooms'
     const expenseAmount = 294.67;
     return(
-        <div className = "expense-item">
+        <Card className = "expense-item">
             <div>{props.expenses.date.toISOString()}</div>
             <div className = "expense-item__description"></div>
             <h2>
                 {props.expenses.title}
             </h2>
             <div className = "expense-item__price">${props.expenses.amount}</div>
-        </div>
+        </Card>
 
     )
 }
