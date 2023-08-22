685df742091e49c146fa374cd6ee56cda1abf600
Jingzhuo <tianboyd@Tians-MacBook-Pro.local>
2023-08-22 15:22:50 -0700

    conditional content rendered

--- src/App.js
+++ src/App.js
@@ -24,21 +24,25 @@ function App() {
   //   console.log("IN App.js");
   //   console.log(expense);
   // };
-  const [filteredYear, setFilteredYear] = useState('2020');
-  const [filterInfoText, setFilterInfoText] = useState('2019, 2021 & 2022')
+  const [filteredYear, setFilteredYear] = useState('2023');
+  const [filterInfoText, setFilterInfoText] = useState('2019, 2020, 2021 & 2022')
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
     if (selectedYear === '2019') {
-      setFilterInfoText('2020, 2021 & 2022')
+      setFilterInfoText('2020, 2021, 2022 & 2023')
     } else if (selectedYear === '2020') {
-      setFilterInfoText('2019, 2021 & 2022')
+      setFilterInfoText('2019, 2021, 2022 & 2023')
     } else if (selectedYear === '2021'){
-      setFilterInfoText('2019, 2020 & 2022')
+      setFilterInfoText('2019, 2020, 2022 & 2023')
+    } else if (selectedYear === '2022'){
+      setFilterInfoText('2019, 2020 , 2021 , 2023')
     } else {
-      setFilterInfoText('2019, 2020 & 2021')
+      setFilterInfoText('2019, 2020, 2021, 2022')
     }
   }
-
+  const filteredExpenses  = expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
   
   return (
     <div>
@@ -46,7 +50,7 @@ function App() {
       <Card className="expenses">
         <ExpensesFilter selected = {filteredYear} onChangeFilter = {filterChangeHandler} />
         <p>Data for year {filterInfoText} is hidden.</p>
-        {expenses.map(i => <ExpenseItems key= {i.id} expenses = {i}/>)}
+        {filteredExpenses.map(i => <ExpenseItems key= {i.id} expenses = {i}/>)}
         {/* <ExpenseItems 
           expenses = {expenses[0]}
         >
