a02e1fbd91dd9a8ad6dee8c25aa3eee6c9863202
Jingzhuo <tianboyd@Tians-MacBook-Pro.local>
2023-08-23 14:36:15 -0700

    final

--- src/components/newExpense/ExpenseForm.js
+++ src/components/newExpense/ExpenseForm.js
@@ -34,7 +34,7 @@ const ExpenseFrom = (props) => {
         event.preventDefault();
         const expenseDate = {
             title: enterdTitle,
-            amount: enterdAmount,
+            amount: +enterdAmount,
             date: new Date(enterdDate)
         };
         // console.log(expenseDate);
